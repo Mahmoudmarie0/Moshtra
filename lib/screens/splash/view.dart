4f1865edc695532fbb32c58414073d607bb78738
Mahmoudmarie0 <mahmoudmarie500@gmail.com>
2023-10-21 18:58:37 +0300

    utils file added

---
+++ lib/screens/splash/view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+
+
+
+
+
+}

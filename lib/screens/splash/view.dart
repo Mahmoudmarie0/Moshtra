c6d19148c20b6b2de190005fd1041715f196ea7e
Mahmoudmarie0 <mahmoudmarie500@gmail.com>
2023-10-28 17:48:49 +0200

    Mahmoud comment add

--- lib/screens/splash/view.dart
+++ lib/screens/splash/view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/widgets.dart';
 // hello from adham
+//hello mahmoud
 class SplashScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     throw UnimplementedError();
+    //mahmoud ahmed
+    var a;
   }
 
 

12e31e90c472958bd6f1540618cb0d83a81987a8
Mahmoudmarie0 <mahmoudmarie500@gmail.com>
2023-11-07 01:36:18 +0200

    comment add

--- lib/screens/login/controller/controller.dart
+++ lib/screens/login/controller/controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-
+//mahm
 class LoginController extends GetxController{
   bool oobscureText=true;
   var formKey = GlobalKey<FormState>();

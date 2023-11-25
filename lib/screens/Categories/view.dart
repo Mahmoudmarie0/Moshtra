e3daafd997d3fb12cd99372971af84a5a6baef30
ADHAM <adham.khalil372002@gmail.com>
2023-11-25 17:31:27 +0200

    Category Screen Added

--- lib/screens/Categories/view.dart
+++ lib/screens/Categories/view.dart
@@ -1,16 +1,80 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../utils/constants/components.dart';
+import 'controller/Controller.dart';
+import 'model.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+CategoriesController categoriesController=Get.put(CategoriesController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(child: Text("CategoriesScreen",style:TextStyle(fontWeight:FontWeight.w600,fontSize: 40) ,)),
-              Center(child: Text("Soon....",style:TextStyle(fontWeight:FontWeight.w400,fontSize: 30) ,)),]));
+
+      return  GetBuilder<CategoriesController>(
+            init: CategoriesController(),
+            builder : (controller)=>
+              ListView.separated(
+                itemBuilder: (context,index)=>buildCatItem(controller.categoriesModel!.data!.data[index]),
+                separatorBuilder: (context,index)=>MyDivider(),
+                itemCount:controller.categoriesModel!.data!.data.length,
+              ),
+
+
+
+
+          );
+      }
+
+
   }
-}
+
+
+  Widget buildCatItem(DataModel model)=>  Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Image(
+            image:
+            NetworkImage
+              (model.image!),
+
+            width: 80.0,
+            height: 80.0,
+            fit: BoxFit.cover,
+
+
+
+          ),
+          SizedBox(
+            width: 20.0,
+          ),
+          Text(model.name!,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+
+            ),),
+          Spacer(),
+          Icon(
+            Icons.arrow_forward_ios,
+          ),
+
+
+
+
+
+
+        ],
+
+
+
+
+      ),
+      );
+
+
+
+
+
+
+

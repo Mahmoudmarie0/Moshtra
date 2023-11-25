5f3f7764ce2cbea89a368355c220a3657742741b
yousefsayed553 <yousefsayed553>
2023-11-25 19:55:34 +0200

    my cart Ui added

--- lib/screens/MyCart/view.dart
+++ lib/screens/MyCart/view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyCartScreen extends StatelessWidget {
   const MyCartScreen({super.key});
@@ -7,10 +8,100 @@ class MyCartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(child: Text("MyCartScreen",style:TextStyle(fontWeight:FontWeight.w600,fontSize: 40) ,)),
-              Center(child: Text("Soon....",style:TextStyle(fontWeight:FontWeight.w400,fontSize: 30) ,)),]));
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text('My Cart'),
+            Spacer(),
+            Text(
+              'Voucher Code',
+              style: TextStyle(
+                color: Color(0xff21D4B4),
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Product(),
+    );
+  }
+
+}
+class Product extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        children: [
+          Container(
+            width: 328.w,
+            height: 120.h,
+            child: Row(
+              children: [
+                Container(
+                    width: 120.w,
+                    height: 120.h,
+                    child: Image(
+                      image: AssetImage('assetName'),
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                        margin: EdgeInsets.symmetric(horizontal: 8),
+                        width: 168.w,
+                        child: Text(
+                          'Loop Silicone Strong Magnetic Watch',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        )),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text('\$15.25'),
+                    ),
+                    Container(
+                      width: 96.w,
+                      height: 32.h,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                              child: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.remove))),
+                          Spacer(),
+                          Expanded(
+                              child: Text(
+                                '1',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )),
+                          Expanded(
+                              child: IconButton(
+                                  onPressed: () {}, icon: Icon(Icons.add)))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Checkbox(value: true, onChanged: (bool? value) {}),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+

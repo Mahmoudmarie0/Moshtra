fdf9d4c26dd3bfc14b9f580a3b889f61fc0e0df8
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-03-09 22:49:54 +0200

    Some enhancment Added

---
+++ lib/screens/Home/widgets/ListViewProduct.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+
+import '../../../models/fav_product_model.dart';
+import '../../../utils/constants/assets.dart';
+import '../../../utils/constants/colors.dart';
+import '../../../utils/custom_text/view.dart';
+import '../../Details/view.dart';
+import '../../Wishlist/database/fav_view_model.dart';
+import '../../Wishlist/view.dart';
+import '../controller/Controller.dart';
+
+ListViewProduct() {
+  return GetBuilder<HomeController>(
+    builder:(controller)=> GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 1/1.20,
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 0.5,
+
+        ),
+        itemCount: controller.productModel.length,
+        itemBuilder: (context,index){
+          return Container(
+            width: MediaQuery.of(context).size.width*.4,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    Get.to(DetailsScreen(controller.productModel[index]));
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24),
+                      color: Colors.white,
+
+                    ),
+
+                    width: MediaQuery.of(context).size.width*.4,
+                    height: 135.h,
+
+                    child: Container(
+                      height:138,
+                      width: 160,
+
+
+                      child: Stack(
+                          alignment: Alignment.center,
+
+                          children:[
+
+                            Image.network(controller.productModel[index].image as String,fit:BoxFit.fill,),
+
+                            GetBuilder<FavViewModel>(
+                              init: Get.put(FavViewModel()),
+                              builder: (controller2) => Align(
+                                  alignment: Alignment.topRight,
+                                  child: GestureDetector(
+                                      onTap: (){
+                                        controller2.addProduct(
+                                            FavProductModel(
+                                                name: controller.productModel[index].name,
+                                                price: controller.productModel[index].price,
+                                                image: controller.productModel[index].image,
+                                                productId: controller.productModel[index].productId
+                                            )
+                                        );
+                                        showSnackBarFun(context);
+                                        print("done");
+
+                                      },
+                                      child: SvgPicture.asset(AssetsPaths.FavIcon,fit: BoxFit.fill,))),
+                            ),
+
+                          ]
+
+
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 9.h,),
+                CustomText(text: controller.productModel[index].name as String,alignment: Alignment.center,fontweight: FontWeight.w600,fontSize: 14, ),
+                SizedBox(height: 5.h,),
+                CustomText(text: controller.productModel[index].sub_description as String,alignment: Alignment.center,color: AppColors.grey,fontweight: FontWeight.w400,maxLine: 1,fontSize: 13,),
+                SizedBox(height: 5.h,),
+                CustomText(text: controller.productModel[index].price.toString()+' EGP' as String,color:AppColors.black,fontweight: FontWeight.w500 ,alignment: Alignment.center,fontSize: 12,),
+
+
+              ],
+            ),
+          );
+
+
+
+        }),
+  );
+}
+showSnackBarFun(context){
+  SnackBar snackBar = SnackBar(
+      dismissDirection: DismissDirection.up,
+      behavior: SnackBarBehavior.floating,
+      duration: Duration.millisecondsPerSecond.milliseconds,
+      elevation: 0.5,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+      ),
+      backgroundColor: AppColors.white,
+      margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).size.height - 200,
+          left: 15,
+          right: 15
+      ),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Icon(
+                Icons.task_alt_rounded,
+                color: AppColors.Green,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'The product has been added to\nyour Favorite',
+                style: TextStyle(
+                  color: AppColors.black,
+                ),
+              )
+            ],
+          ),
+          GestureDetector(
+            child: Text('View Favorite'
+              ,style: TextStyle(
+                  color: AppColors.orange
+              ),
+            ),
+            onTap: (){ Get.to(MyFavScreen()); },
+          )
+        ],
+      )
+
+  );
+
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
---
+++ lib/screens/Home/widgets/ListviewCategory.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../controller/Controller.dart';
+
+Widget ListViewCategory() {
+  return GetBuilder<HomeController>(
+    builder:(controller)=> Container(
+      height: 100.h,
+      child: ListView.separated(
+        itemCount:4,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context,index){
+          return Column(
+            children: [
+              SingleChildScrollView(
+                child: Container(
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey.shade100,
+
+                  ),
+                  height: 60.h,
+                  width: 76.w,
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image .network(controller.categoryModel[index].image as String),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 1.h,),
+              Text(controller.categoryModel[index].name as String,style: TextStyle(fontWeight: FontWeight.w800,fontSize: 10.sp,color: AppColors.black),),
+            ],
+          );
+
+
+
+        }, separatorBuilder: ( context,  index) =>SizedBox(
+        width: 20,
+      ),
+
+
+
+      ),
+    ),
+  );
+}

29dc44cd17174d48c2a4abf6d6ee36f992f75685
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-05-07 19:22:17 +0300

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
-
-
-
-
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_navigation/src/routes/transitions_type.dart';
 import 'package:moshtra/firebase_options.dart';
 import 'package:moshtra/screens/splash/view.dart';
-import 'package:moshtra/service/home_service.dart';
 import 'package:moshtra/service/stripe_payment/stripe_Keys.dart';
 import 'package:moshtra/utils/constants/localizations.dart';
 import 'package:moshtra/utils/translation/translation.dart';
@@ -24,65 +15,58 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'global_controller/my_bindings.dart';
 
-SharedPreferences ? sharedPref;
+SharedPreferences? sharedPref;
 void main() async {
-
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   ResponsiveSizingConfig.instance.setCustomBreakpoints(
-    const ScreenBreakpoints(desktop: 800, tablet: 450, watch: 200),);
-
-
-//s;d;lkkd
-
+    const ScreenBreakpoints(desktop: 800, tablet: 450, watch: 200),
+  );
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   //await FirebaseNotification().initNotification();
-  runApp( MyApp());
-  sharedPref=await SharedPreferences.getInstance();
+  runApp(MyApp());
+  sharedPref = await SharedPreferences.getInstance();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  //DioHelper.init();
-  Stripe.publishableKey=ApiKeys.publishableKey;
+  Stripe.publishableKey = ApiKeys.publishableKey;
 }
 
 class MyApp extends StatelessWidget {
-
   //const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        useInheritedMediaQuery: true,
+      useInheritedMediaQuery: true,
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context , child) {
+      builder: (context, child) {
         return GetMaterialApp(
-          initialBinding: MyBindings(),
-          debugShowCheckedModeBanner: false,
-          defaultTransition: Transition.leftToRight,
-          title: 'Moshtra',
-          theme: ThemeData(
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-            fontFamily: "IBMPlex",
-          ),
-          builder: EasyLoading.init(),
-          locale: const Locale("en", "US"),
-          translations: AppTranslation(),
-          supportedLocales: supportedLocales,
-          localizationsDelegates: localizationsDelegate,
-          localeResolutionCallback: localeResolutionCallback,
-
-          home:Directionality(
-            textDirection: Get.locale?.languageCode=="en"?TextDirection.ltr:TextDirection.rtl,
-            child:  child!,
-          )
-        );
+            initialBinding: MyBindings(),
+            debugShowCheckedModeBanner: false,
+            defaultTransition: Transition.leftToRight,
+            title: 'Moshtra',
+            theme: ThemeData(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              fontFamily: "IBMPlex",
+            ),
+            builder: EasyLoading.init(),
+            locale: const Locale("en", "US"),
+            translations: AppTranslation(),
+            supportedLocales: supportedLocales,
+            localizationsDelegates: localizationsDelegate,
+            localeResolutionCallback: localeResolutionCallback,
+            home: Directionality(
+              textDirection: Get.locale?.languageCode == "en"
+                  ? TextDirection.ltr
+                  : TextDirection.rtl,
+              child: child!,
+            ));
       },
       child: SplashScreen(),
     );
-
   }
 }
-
-
